83f8924754a735c3b09a0ff97f0b70b8b151b8bc
omerdaube <102986147+omerdaube@users.noreply.github.com>
2024-02-06 20:07:40 +0200

    Create page_alias.h

---
+++ include/linux/page_alias.h
@@ -0,0 +1,11 @@
+#ifndef __LINUX_PAGE_ALIAS_H
+#define __LINUX_PAGE_ALIAS_H
+
+#include <linux/jump_label.h>
+
+#ifdef CONFIG_PAGE_ALIAS
+
+extern struct page_ext_operations page_alias_ops;
+
+#endif
+#endif
