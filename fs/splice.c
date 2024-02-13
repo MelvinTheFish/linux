0e05cf2f9ecd5c2ea9560cea9ce905cc63ea3275
omerdaube <102986147+omerdaube@users.noreply.github.com>
2024-02-13 20:17:12 +0200

    Update splice.c

--- fs/splice.c
+++ fs/splice.c
@@ -131,7 +131,7 @@ static void page_cache_pipe_buf_release(struct pipe_inode_info *pipe,
 	// put_page(buf->page);
 	// //alias_page_close(buf_page);
 	// buf->flags &= ~PIPE_BUF_FLAG_LRU;
-	put_page(buf->page);
+	put_page(page);
 	buf->flags &= ~PIPE_BUF_FLAG_LRU;
 }
 
