fbfb47f0c55985a7aad53c5ae92cf58e7ac8b006
omerdaube <102986147+omerdaube@users.noreply.github.com>
2024-02-13 12:39:30 +0200

    Update page_alias.h

--- include/linux/page_alias.h
+++ include/linux/page_alias.h
@@ -4,9 +4,14 @@
 #include <linux/jump_label.h>
 
 #ifdef CONFIG_PAGE_ALIAS
-
+extern void __create_page_alias(struct page *page);
 extern struct page_ext_operations page_alias_ops;
 static struct page_alias* get_page_alias(struct page *page);
+
+static inline void create_page_alias(struct page *page)
+{
+        __create_page_alias(page);
+}
 
 #endif
 #endif
