8bd64d7f6a93aa7ee93b586d1a078220ef466250
omerdaube <102986147+omerdaube@users.noreply.github.com>
2024-02-13 13:32:54 +0200

    Update page_alias.h

--- include/linux/page_alias.h
+++ include/linux/page_alias.h
@@ -9,8 +9,7 @@ extern struct page_ext_operations page_alias_ops;
 
 static inline void set_page_alias(struct page *page)
 {
-      //  __set_page_alias(page);
-        printk(KERN_ERR "omer and nizan: in set page_alias");
+       __set_page_alias(page);
 }
 
 #endif
