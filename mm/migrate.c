c6278413f5d7d1a4dc00b14eec7d1f96de30f7ba
Nizan Kafman-Raz nizan.kafman@campus.technion.ac.il <nizan.kafman@csl-tapuz22.cslcs.technion.ac.il>
2024-07-07 19:20:57 +0300

    bla

--- mm/migrate.c
+++ mm/migrate.c
@@ -798,8 +798,10 @@ int folio_migrate_copy(struct folio *newfolio, struct folio *folio)
 	}
 	folio_copy(newfolio, folio);
 	if (pinned){
-		if (kernel_pinned && kernel_migrate_pinned_page_commit(newfolio, folio) != MIGRATEPAGE_SUCCESS)
-			return -EPINMIGF;//should by Ebusy
+		if (kernel_pinned){
+			if(kernel_migrate_pinned_page_commit(newfolio, folio) != MIGRATEPAGE_SUCCESS)
+				return -EPINMIGF;//should by Ebusy
+		}
 		else
 			call_dma_migrate_page(page, false, newfolio); // false for not prepare, should also include return value later
 	}
